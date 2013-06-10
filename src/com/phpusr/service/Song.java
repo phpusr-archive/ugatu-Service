fc10c646036b6b47e2d313cb77acd15193ed1fd5
phpusr <phpusr@gmail.com>
2013-06-10 11:57:03 +0600

    Доделал класс Музыкальная композиция

--- src/com/phpusr/service/Song.java
+++ src/com/phpusr/service/Song.java
@@ -15,7 +15,7 @@ import java.io.IOException;
  */
 
 /**
- * TODO Исполнитель, Альбом, Имя файла (Выводить его, если все ост-е пусто)
+ * Музыкальная композиция (Имя файла, Название, Исполнитель, Альбом, Битрейт)
  */
 public class Song {
     private MP3File mp3File;
@@ -24,11 +24,58 @@ public class Song {
         mp3File = new MP3File(file);
     }
 
+    /** Имя файла */
+    public String getFileName() {
+        return mp3File.getMp3file().getName();
+    }
+
+    /** Название композиции */
     public String getTitle() {
-        if (mp3File != null && mp3File.getID3v1Tag() != null) {
+        if (isIssetTags()) {
             return mp3File.getID3v1Tag().getSongTitle() != null ? mp3File.getID3v1Tag().getSongTitle() : mp3File.getID3v2Tag().getSongTitle();
         } else {
             return null;
+        }
+    }
+
+    /** Исполнитель */
+    public String getArtist() {
+        if (isIssetTags()) {
+            return mp3File.getID3v1Tag().getLeadArtist() != null ? mp3File.getID3v1Tag().getLeadArtist() : mp3File.getID3v2Tag().getLeadArtist();
+        } else {
+            return null;
+        }
+    }
+
+    /** Альбом */
+    public String getAlbum() {
+        if (isIssetTags()) {
+            return mp3File.getID3v1Tag().getAlbumTitle() != null ? mp3File.getID3v1Tag().getAlbumTitle() : mp3File.getID3v2Tag().getAlbumTitle();
+        } else {
+            return null;
+        }
+    }
+
+    /** Битрейт */
+    public int getBitRate() {
+        if (isIssetTags()) {
+            return mp3File.getBitRate();
+        } else {
+            return 0;
+        }
+    }
+
+    /** Проверка, есть ли теги в файле */
+    private boolean isIssetTags() {
+        return mp3File != null && (mp3File.getID3v1Tag() != null || mp3File.getID3v2Tag() != null);
+    }
+
+    @Override
+    public String toString() {
+        if (isIssetTags()) {
+            return getArtist() + " - " + getTitle() + " (" + getAlbum() + ")";
+        } else {
+            return getFileName();
         }
     }
 }
