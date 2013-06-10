1fbb1ab669456dfbc9505059c7627b20a4059fdd
phpusr <phpusr@gmail.com>
2013-06-10 14:43:00 +0600

    Вынес аргументы в Файл конфигурации, мелкие правки в PhpusrWrapperListener

--- src/com/phpusr/service/PhpusrWrapperListener.java
+++ src/com/phpusr/service/PhpusrWrapperListener.java
@@ -4,31 +4,33 @@ import org.tanukisoftware.wrapper.WrapperListener;
 import org.tanukisoftware.wrapper.WrapperManager;
 
 /**
- * Created with IntelliJ IDEA.
- * User: phpusr
+ * @author phpusr
  * Date: 08.06.13
  * Time: 11:35
- * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * Оболочка для сервиса
  */
 public class PhpusrWrapperListener implements WrapperListener {
 
-    private Thread mp3Tag2Html;
+    private Thread service;
 
     private PhpusrWrapperListener() {}
 
     @Override
     public Integer start(String[] args) {
-        System.out.println(">>Program start...");
-        mp3Tag2Html = new MP3Tag2Html("../html/report.html", "f:\\Music\\DL\\Superbus - Sunset (2012)");
-        mp3Tag2Html.start();
+        System.out.println(">>Service start...");
+        service = new MP3Tag2Html(args[0], args[1]);
+        service.start();
 
         return null;
     }
 
     @Override
     public int stop(int exitCode) {
-        System.out.println(">>Program end...");
-        mp3Tag2Html.interrupt();
+        System.out.println(">>Service end...");
+        service.interrupt();
 
         return exitCode;
     }
